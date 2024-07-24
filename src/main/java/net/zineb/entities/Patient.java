fdd4f4dcc5b468db638b0e36e2e567922f0fe4bc
Zineb Kourifa <zinebkourifa@gmail.com>
2024-07-24 18:09:19 +0100

    V1

---
+++ src/main/java/net/zineb/entities/Patient.java
@@ -0,0 +1,13 @@
+package net.zineb.entities;
+
+import java.util.Date;
+
+public class Patient {
+
+    private Long id;
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+    private  int score;
+    private  boolean malade;
+}
